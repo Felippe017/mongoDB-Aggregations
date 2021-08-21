161d35974eb7bbc5add12e3aaf5e0703ab8ae5ae
esmaben <felippecorrea2010@gmail.com>
2021-08-21 12:00:32 -0300

    DONE:  desafio7

---
+++ challenges/desafio7.js
@@ -0,0 +1,30 @@
+db.movies.aggregate([
+  {
+    $match: {
+      cast: { $exists: true },
+      languages: { $all: ["English"] },
+    },
+  },
+  {
+    $unwind: "$cast",
+  },
+  {
+    $group: {
+      _id: "$cast",
+      numeroFilmes: { $count: { } },
+      meIMDB: { $avg: "$imdb.rating" },
+    },
+  },
+  {
+    $sort: {
+      numeroFilmes: -1,
+      _id: 1,
+    },
+  },
+  {
+    $project: {
+      numeroFilmes: 1,
+      mediaIMDB: { $round: ["$meIMDB", 1] },
+    },
+  },
+]);
