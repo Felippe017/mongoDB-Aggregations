ed6aa7e3cf71434e3472d6c56c3e898a7d97c47f
esmaben <felippecorrea2010@gmail.com>
2021-08-22 15:26:30 -0300

    DONE:  desafio10

---
+++ challenges/desafio10.js
@@ -0,0 +1,33 @@
+use("aggregations");
+db.trips.aggregate([
+  {
+    $addFields: {
+      diferencaTime: {
+        $subtract: ["$stopTime", "$startTime"],
+      },
+    },
+  },
+  {
+    $group: {
+      _id: "$usertype",
+      mediaViagem: { $avg: "$diferencaTime" },
+    },
+  },
+  {
+    $addFields: {
+      hora: { $divide: ["$mediaViagem", 1000 * 60 * 60] },
+    },
+  },
+  {
+    $sort: {
+      hora: 1,
+    },
+  },
+  {
+    $project: {
+      _id: 0,
+      tipo: "$_id",
+      duracaoMedia: { $round: ["$hora", 2] },
+    },
+  },
+]);
